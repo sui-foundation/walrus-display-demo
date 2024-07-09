31dc2269c65826cc6efcdbc125dc21c84346f565
Daniel Leavitt <daniel.7039@hotmail.com>
2024-07-09 10:38:04 -0700

    functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aggregator-devnet.walrus.space',
+        port: '',
+        pathname: '/v1/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
